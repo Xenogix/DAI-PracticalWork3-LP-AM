d081055871086ae8fd78ea0e45dd2217ca70cfd6
Lionel Pollien <lionel.pollien@heig-vd.ch>
2023-12-12 17:30:00 +0100

    beginning of multicast

--- src/main/java/ch/heigvd/client/net/UpdateClientHandler.java
+++ src/main/java/ch/heigvd/client/net/UpdateClientHandler.java
@@ -1,4 +1,41 @@
 package ch.heigvd.client.net;
 
+import ch.heigvd.client.ClientStorage;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 public class UpdateClientHandler {
+
+    private final ClientStorage clientStorage = ClientStorage.getInstance();
+
+    public void update(){
+
+        try(MulticastSocket socket = new MulticastSocket()){
+            InetAddress multicastAddress = InetAddress.getByName();
+            InetSocketAddress group = new InetSocketAddress();
+            NetworkInterface networkInterface = NetworkInterface.getByName();
+            socket.joinGroup(group, networkInterface);
+
+            socket.joinGroup();
+
+            byte[] receiveData = new byte[1024];
+
+            while(true){
+                DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
+
+                socket.receive(packet);
+
+                String message = new String(
+                        packet.getData(),
+                        packet.getOffset(),
+                        packet.getLength(),
+                        StandardCharsets.UTF_8
+                );
+            }
+        }catch(IOException exception){
+            throw new RuntimeException(exception); //Todo
+        }
+    }
 }
--- src/main/java/ch/heigvd/server/net/ServerCommandEndpoint.java
+++ src/main/java/ch/heigvd/server/net/ServerCommandEndpoint.java
@@ -6,8 +6,6 @@ import java.net.DatagramSocket;
 public class ServerCommandEndpoint {
     private static final short PORT = 23102;
 
-
-
     public void start(){
         try(DatagramSocket socket = new DatagramSocket(PORT)){
 
@@ -15,10 +13,7 @@ public class ServerCommandEndpoint {
                 byte[] data = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(data, data.length);
                 socket.receive(packet);
-
-
                 new Thread(new ServerResponder(packet, socket)).start();
-
 
             }
         }catch (Exception e){
