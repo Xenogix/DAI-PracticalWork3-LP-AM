d88599ad1a8964d154bc65554f75d9ffb11b0088
Lionel Pollien <lionel.pollien@heig-vd.ch>
2023-12-13 10:40:29 +0100

    Gui branch - beginning

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -1,7 +1,10 @@
 package ch.heigvd;
 
+import ch.heigvd.shared.gui.Gui;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
     }
 }
---
+++ src/main/java/ch/heigvd/shared/gui/Gui.java
@@ -0,0 +1,31 @@
+package ch.heigvd.shared.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Gui extends JPanel implements ActionListener {
+
+    //Board properties
+    private final int WIDTH = 300;
+    private final int HEIGHT = 300;
+    private final int SQUARE_SIZE = 10;
+    private final int SQUARE_SUM = 900;
+
+    public Gui(){
+        initBoard();
+    }
+
+    public void initBoard() {
+        setBackground(Color.BLACK);
+        setFocusable(true); //Need the focus to receive the command probably useless cause in manage elsewhere
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
