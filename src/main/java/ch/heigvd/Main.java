19d52d2758a750ea9d38850637ba3c75915d106a
Xenogix <arthur.menetrey@icloud.com>
2023-12-14 15:05:05 +0100

    Removed sharder folder

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -1,6 +1,5 @@
 package ch.heigvd;
 
-import ch.heigvd.shared.gui.Gui;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- src/main/java/ch/heigvd/gui/Gui.java
+++ src/main/java/ch/heigvd/gui/Gui.java
@@ -1,4 +1,4 @@
-package ch.heigvd.shared.gui;
+package ch.heigvd.gui;
 
 import javax.swing.*;
 import java.awt.*;
